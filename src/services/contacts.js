3c39f2519640b480bce9394d37a03adf6474c781
lilshudd <max.fedorak10@gmail.com>
2024-06-29 09:58:33 +0300

    rld

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,13 +4,13 @@ const getContacts = async (req, res) => {
   try {
     const contacts = await Contact.find();
     res.json({
-      status: 'success',
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ status: 500, message: 'Server error' });
   }
 };
 
@@ -21,18 +21,19 @@ const getContactById = async (req, res) => {
 
     if (!contact) {
       return res.status(404).json({
+        status: 404,
         message: `Contact with id ${contactId} not found`,
       });
     }
 
     res.json({
-      status: 'success',
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ status: 500, message: 'Server error' });
   }
 };
 
