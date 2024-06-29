422681e1ace8783b32240ba506de37b0a8d047a5
lilshudd <max.fedorak10@gmail.com>
2024-06-29 17:03:15 +0300

    Update errorHandlers.js

--- src/middlewares/errorHandlers.js
+++ src/middlewares/errorHandlers.js
@@ -2,10 +2,7 @@ const errorHandler = (err, req, res) => {
   const statusCode = err.status || 500;
   const message = err.message || 'Something went wrong';
 
-  res.status(statusCode).json({
-    status: statusCode,
-    message: message,
-  });
+  res.status(statusCode).json({ error: { message, statusCode } });
 };
 
 module.exports = { errorHandler };
