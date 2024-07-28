8be9d8656b04a5bc4053a55b67da95f7beab43b6
lilshudd <max.fedorak10@gmail.com>
2024-07-28 23:19:36 +0300

    first

--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const pino = require('pino-http')();
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 const authRouter = require('./routers/auth');
 const contactsRouter = require('./routers/contacts');
 const { errorHandler } = require('./middlewares/errorHandlers');
 const notFoundHandler = require('./middlewares/notFoundHandler');
 const authenticate = require('./middlewares/authenticate');
+
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
 
 const setupServer = () => {
   const app = express();
@@ -18,6 +23,7 @@ const setupServer = () => {
 
   app.use('/auth', authRouter);
   app.use('/contacts', authenticate, contactsRouter);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(notFoundHandler);
   app.use(errorHandler);
